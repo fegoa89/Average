c2b23f1e6dfba8e97867c43c31fccd1c17ae7942
Fernando <fgonzalezaguilera@gmail.com>
2014-12-26 17:55:54 +0100

    added mode and unique mode spec

--- spec/average_spec.rb
+++ spec/average_spec.rb
@@ -3,9 +3,12 @@ require 'spec_helper'
 describe Average do
 
   let(:int_array) { [3,4,5] }
-  let(:even_int_array) { [3,2,5,4] }
 
   let(:mixed_array) { [3, 4.0, 5, 2.0, 3, 1.0] }
+
+  let(:mode_array) { [1,1,2,2,3,4] }
+
+  let(:mode_mixed_array) { [1,1.0,2,2.0,3.0,4] }
 
   describe '#mean' do
 
@@ -45,4 +48,35 @@ describe Average do
 
   end
 
+  describe '#mode' do
+
+    context 'with a array of integers' do
+      it 'should calculate the correct mode' do
+        expect(mode(mode_array)).to eq([1, 2])
+      end
+    end
+
+    context 'with a mixed array of integers and floats' do
+      it 'should calculate the correct mode' do
+        expect(mode(mode_mixed_array)).to eq([1, 2])
+      end
+    end
+
+  end
+
+  describe '#unique_mode' do
+
+    context 'with a array of integers' do
+      it 'should calculate the correct mode returning only one mode' do
+        expect(unique_mode(mode_array)).to eq(1)
+      end
+    end
+
+    context 'with a mixed array of integers and floats' do
+      it 'should calculate the correct mode returning only one mode' do
+        expect(unique_mode(mode_mixed_array)).to eq(1)
+      end
+    end
+
+  end
 end
