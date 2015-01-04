022ee0ddc6f2654d9f6c36c047240f7ab513b5cb
Fernando <fgonzalezaguilera@gmail.com>
2015-01-04 22:46:27 +0100

    added new cases to median_spec.rb

--- spec/median_spec.rb
+++ spec/median_spec.rb
@@ -2,11 +2,15 @@ require 'spec_helper'
 
 describe Average do
 
-  let(:int_array) { [3,4,5] }
+  let(:int_array)          { [3, 4, 5]                }
 
-  let(:mixed_array) { [3, 4.0, 5, 2.0, 3, 1.0] }
+  let(:array)              { [1, 1, 2, 2, 3, 4]       }
 
-  let(:mode_array) { [1,1,2,2,3,4] }
+  let(:mixed_array)        { [3, 4.0, 5, 2.0, 3, 1.0] }
+
+  let(:array_to_average)   { [3, 4, 7, 8, 10, 12]     }
+
+  let(:mode_array_not_pair) { [1, 1, 2, 2, 3, 4, 5]   }
 
   describe '#median' do
 
@@ -28,6 +32,36 @@ describe Average do
       end
     end
 
+  end
+
+  describe '#get_median' do
+    context 'with an even array' do
+      it 'should give back the middle value' do
+        expect(get_median(array)).to eq(2.0)
+      end
+    end
+
+    context 'with a _not_ even array' do
+      it 'should give back the the element on the middle' do
+        expect(get_median(mode_array_not_pair)).to eq(2)
+      end
+    end
+  end
+
+  describe '#middle_item' do
+    context 'with a sorted array like [1,1,2,2,3,4]' do
+      it 'should give back the middle value' do
+        expect(middle_item(array)).to eq(3)
+      end
+    end
+  end
+
+  describe '#middle_items_to_average' do
+    context 'with an even array' do
+      it 'should give back two middle items' do
+        expect(middle_items_to_average(array_to_average)).to eq([7, 8])
+      end
+    end
   end
   
 end
