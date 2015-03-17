f7dea371ffe98067b826a1f0379775ceff0b97d6
Fernando <fgonzalezaguilera@gmail.com>
2015-03-17 22:04:41 +0100

    added some methods to mode module

--- lib/average/mode.rb
+++ lib/average/mode.rb
@@ -1,19 +1,30 @@
 def get_mode(array)
   return if not valid_array?(array)
-  array  = clean_array(array)
-  result = repetition_hash(array)
-  result[:hash_result].select { |key, value| value == result[:max_repetition] }.keys.collect { |float| float.to_i }
+  result = repetition_hash( clean_array(array) )
+  if build_hash_result(result).count > 1
+    # It contains more than one mode
+    build_hash_result(result).keys.collect { |float| float.to_i }
+  else
+    # It contains only one mode
+    build_hash_result(result).keys.first
+  end
 end
 
 def repetition_hash(array)
   return if not valid_array?(array)
-  result = array.inject({}) { |key, value| key[value] = array.count(value.to_f); key }
-  { hash_result: result, max_repetition: result.values.max }
+  { hash_result: build_repetition_result(array), max_repetition: build_repetition_result(array).values.max }
+end
+
+def build_hash_result(result)
+  result[:hash_result].select { |key, value| value == result[:max_repetition] }
+end
+
+def build_repetition_result(array)
+   array.inject({}) { |key, value| key[value] = array.count(value.to_f); key }
 end
 
 def unique_mode(array)
   # only for Ruby versions bigger than 1.8.7 -> http://apidock.com/ruby/v1_8_7_72/Enumerable/max_by
   return if !valid_array?(array)
-  array  = clean_array(array)
-  array.max_by { |x| array.count(x) }
+  clean_array(array).max_by { |x| clean_array(array).count(x) }
 end
